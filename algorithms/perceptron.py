ec3a128b1e9bdf2737deb9d4af5af1043e716732
ybrenning <yannickbrenning2@gmail.com>
2023-06-26 15:05:55 +0200

    Finish perceptron implementation

--- algorithms/perceptron.py
+++ algorithms/perceptron.py
@@ -1,4 +1,7 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
 
 
 class Perceptron:
@@ -28,7 +31,7 @@ class Perceptron:
                 self.b += update
 
     def predict(self, X):
-        linear_output = np.dot(X, self.w) + self.bias
+        linear_output = np.dot(X, self.w) + self.b
         y_pred = self.activation_func(linear_output)
         return y_pred
 
@@ -37,4 +40,36 @@ class Perceptron:
 
 
 if __name__ == "__main__":
-    ...
+    X, y = datasets.make_blobs(
+        n_samples=150, n_features=2, centers=2, cluster_std=1.05, random_state=2
+    )
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=123
+    )
+
+    p = Perceptron(learning_rate=0.01, n_iters=1000)
+    p.fit(X_train, y_train)
+    preds = p.predict(X_test)
+
+    acc = np.sum(preds == y_test) / len(y_test)
+
+    print(acc)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    plt.scatter(X_train[:, 0], X_train[:, 1], marker="o", c=y_train)
+
+    x0_1 = np.amin(X_train[:, 0])
+    x0_2 = np.amax(X_train[:, 0])
+
+    x1_1 = (-p.w[0] * x0_1 - p.b) / p.w[1]
+    x1_2 = (-p.w[0] * x0_2 - p.b) / p.w[1]
+
+    ax.plot([x0_1, x0_2], [x1_1, x1_2], "k")
+
+    ymin = np.amin(X_train[:, 1])
+    ymax = np.amax(X_train[:, 1])
+    ax.set_ylim([ymin - 3, ymax + 3])
+
+    plt.show()
